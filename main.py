db25f378244cd9fcbe7fa43bf9d15d530e96b446
h1ro <treepa4@asuma.ru>
2023-02-27 19:44:26 +0300

    в боте /start /help рабочие библиотека и кфг импортированы полностью

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import telebot
 import cfg
+
+
+
+
+bot = telebot.TeleBot(cfg.token)
+
+
+#команды '/start' '/help'
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'ИДИ НАХУЙ')
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Че тупой шо ль иди нахуй пень ебаный')
+
+
+# распознования остального как обычный текст
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
