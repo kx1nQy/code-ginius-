994da88855e50a4c8c8ba3ec143ca61011dae195
kx1nQy <loginovt13@ya.ru>
2023-03-01 23:00:19 +0300

    В КОДЕ НЧЕГО ОСОБОГО НЕТ работай с пред идущей версией!!! Я разочарован в жизни видемо придется отключать голову, а не работать с видео на ютуб.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import telebot
 from telebot import types
+import sqlite3 #импортировал херь для работы с базой данных
 
 TOKEN = '6255702149:AAGYcZtZxBx3VSJps6h2ynH0q2Rx0zpkFQM'
 
 bot = telebot.TeleBot(TOKEN)
 
+db = sqlite3.connect('Notes.db')# Создал файл где будут хранится данные
 
+
+db.close()
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton('список команд')
-    item2 = types.KeyboardButton('Заметки')
+    #item1 = types.KeyboardButton('Список команд') Не вижу в ней смысла
+    item2 = types.KeyboardButton('Добавить заметку')
     item3 = types.KeyboardButton('Мои заметки')
     item0 = types.KeyboardButton('Пошел на хуй, не придумал')
-    markup.add(item1, item2, item3, item0)
+    markup.add(item2, item3, item0)
 
     bot.reply_to(message, 'Пpивет, {0.first_name}!'.format(message.from_user),  reply_markup=markup)
 
