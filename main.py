ca22850b4c38731eefd91e76ac36f37357135d97
h1ro <treepa4@asuma.ru>
2023-04-10 21:12:09 +0300

    попытался перевести бота на асинхрон txt.py нахуй не нужная херь UPD: почему то он выдает странные ошибки после завершения работы, но похуй. видимо документацию писали дауны

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import telebot
 from telebot import types
-import sqlite3 #импортировал херь для работы с базой данных
+from telebot.async_telebot import AsyncTeleBot
+from txt import txtHi
 
 TOKEN = '6255702149:AAGYcZtZxBx3VSJps6h2ynH0q2Rx0zpkFQM'
 
-bot = telebot.TeleBot(TOKEN)
-
-db = sqlite3.connect('Notes.db')# Создал файл где будут хранится данные
+bot = AsyncTeleBot(TOKEN)
 
 
-db.close()
 @bot.message_handler(commands=['start'])
-def start(message):
+async def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    #item1 = types.KeyboardButton('Список команд') Не вижу в ней смысла
-    item2 = types.KeyboardButton('Добавить заметку')
+    item1 = types.KeyboardButton('список команд')
+    item2 = types.KeyboardButton('Заметки')
     item3 = types.KeyboardButton('Мои заметки')
     item0 = types.KeyboardButton('Пошел на хуй, не придумал')
-    markup.add(item2, item3, item0)
+    markup.add(item1, item2, item3, item0)
 
-    bot.reply_to(message, 'Пpивет, {0.first_name}!'.format(message.from_user),  reply_markup=markup)
+    await bot.reply_to(message, 'Пpивет, {0.first_name}, я бот без имени, Так как у того кто меня придумал нет фантазии..\n...зато блять есть эвтаназия, с явным ожиданием катарсиса'.format(message.from_user),  reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
-def hutinpuylo(message):
-    if message.text == 'Пошел на хуй, не придумал':
-        bot.send_message(message.chat.id, 'Простите, господин, это рофл? Просто если это не рофл, то я твоего деда нахуй нахуй садил')
+async def hutinpuylo(message):
+     if message.text == 'Пошел на хуй, не придумал':
+        await bot.send_message(message.chat.id, 'Простите, господин, это рофл? Просто если это не рофл, то я твоего деда нахуй нахуй садил')
 
+import asyncio
+asyncio.run(bot.polling())
 
-bot.polling(none_stop = True)
